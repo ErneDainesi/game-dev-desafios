9aec1f75605f712a58cc1c670d4802fb439b81a7
ErneDainesi <ernestodainesiperrot@gmail.com>
2022-07-28 20:18:17 -0300

    Add bullet life time and functionality to duplicate bullet scale

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 5f;
     public Vector3 direction = new Vector3(0, 0.5f, 1f);
+    public float lifeTime = 3f;
     private Vector3 scale = new Vector3(0.5f, 0.5f, 0.5f);
     private Vector3 position = new Vector3(0.02f, 1.09f, 1.6f);
     
@@ -13,15 +14,25 @@ public class Bullet : MonoBehaviour
     {
         transform.localScale = scale;
         transform.position = position;
+        Invoke("DestroyBullet", lifeTime);
     }
 
     void Update()
     {
         Move();
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.localScale = scale * 2;
+        }
     }
     
     private void Move()
     {
         transform.position += direction * (Time.deltaTime * speed);
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
     }
 }
